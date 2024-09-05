0d66e10f23e923cac3d8c35020e625a9188f115c
Luccas Thomaz Pereira Lavrador <108094478+Luccas-thomaz@users.noreply.github.com>
2024-09-05 18:58:51 -0300

    aula 4

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log(dados);
+function search() {
+  // Função para realizar a pesquisa e exibir os resultados
+  let section = document.getElementById("resultados-pesquisa"); // Obtém a seção onde os resultados serão exibidos
+  let resultado_temp = ""; // Variável para armazenar os resultados temporariamente
+
+  // Itera sobre os dados da pesquisa
+  for (let dado of dados) {
+    // Cria um novo elemento div para cada resultado
+    resultado_temp += `
+      <div class="item-resultado">
+        <p></p>
+        <h2><img src="${dado.img}" width="100" height="100"><a href="${dado.link}" target="_blank">   ${dado.titulo}</a></h2>
+        <p class="descricao-meta">${dado.descrição}</p>
+      </div>
+    `;
+  }
+
+  // Atribui os resultados montados à seção
+  section.innerHTML = resultado_temp;
+}
--- dados.js
+++ dados.js
@@ -4,28 +4,36 @@ let dados = [
         descrição: "O Ironclad é um guerreiro bruto e implacável, que usa sua força bruta e armadura pesada para esmagar seus inimigos.",
         habilidades_iniciais: [ "Strike","Defend", "Bash"],
         pontos_fortes: "Alta saúde, cartas de força",
-        pontos_fracos: "Alto custo de energia"
+        pontos_fracos: "Alto custo de energia",
+        link:"https://slay-the-spire.fandom.com/wiki/Ironclad",
+        img:"imgs/Ironclad.jpg"
     },
     {
         titulo: "Silent",
         descrição: "A Silent é uma assassina ágil e letal, que se move pelas sombras e elimina seus inimigos com precisão mortal.",
         habilidades_iniciais: ["Strike", "Defend", "Survivor", "Neutralize"],
         pontos_fortes: "manipulação de mão, veneno",
-        pontos_fracos: "Pouca saúde, deck inicial maior"
+        pontos_fracos: "Pouca saúde, deck inicial maior",
+        link:"https://slay-the-spire.fandom.com/wiki/Silent",
+        img:"imgs/Silent.jpg"
     },
     {
         titulo: "Defect",
         descrição: "O Defect é um ser sintético com a capacidade de manipular a energia, usando orbes para atacar seus inimigos.",
         habilidades_iniciais: ["Strike", "Defend","Zap","Dual Cast"],
         pontos_fortes: "Versatilidade, orbes poderosas, defesa",
-        pontos_fracos: "Curva de aprendizado íngreme, dependência de orbes"
+        pontos_fracos: "Curva de aprendizado íngreme, dependência de orbes",
+        link:"https://slay-the-spire.fandom.com/wiki/Defect",
+        img:"imgs/Defect.jpg"
     },
     {
         titulo: "Watcher",
     descrição: "A Watcher é um ser místico que pode invocar o poder de diferentes deuses para auxiliar em suas batalhas.",
     habilidades_iniciais: ["Strike", "Defend","Eruption", "Vigilance"],
     pontos_fortes: "Versatilidade, cartas poderosas",
-    pontos_fracos: "Curva de aprendizado íngreme"
+    pontos_fracos: "Curva de aprendizado íngreme",
+    link:"https://slay-the-spire.fandom.com/wiki/Watcher",
+    img:"imgs/Watcher.jpg"
     }
 ];
 
