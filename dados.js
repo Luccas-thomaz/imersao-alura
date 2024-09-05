33d005182d84c8fd65a287cf8e97fa324abfcf70
Luccas Thomaz Pereira Lavrador <108094478+Luccas-thomaz@users.noreply.github.com>
2024-09-05 01:01:27 -0300

    aula 3
    mudei para slay the spire

---
+++ dados.js
@@ -0,0 +1,31 @@
+let dados = [
+    {
+        titulo: "Ironclad",
+        descrição: "O Ironclad é um guerreiro bruto e implacável, que usa sua força bruta e armadura pesada para esmagar seus inimigos.",
+        habilidades_iniciais: [ "Strike","Defend", "Bash"],
+        pontos_fortes: "Alta saúde, cartas de força",
+        pontos_fracos: "Alto custo de energia"
+    },
+    {
+        titulo: "Silent",
+        descrição: "A Silent é uma assassina ágil e letal, que se move pelas sombras e elimina seus inimigos com precisão mortal.",
+        habilidades_iniciais: ["Strike", "Defend", "Survivor", "Neutralize"],
+        pontos_fortes: "manipulação de mão, veneno",
+        pontos_fracos: "Pouca saúde, deck inicial maior"
+    },
+    {
+        titulo: "Defect",
+        descrição: "O Defect é um ser sintético com a capacidade de manipular a energia, usando orbes para atacar seus inimigos.",
+        habilidades_iniciais: ["Strike", "Defend","Zap","Dual Cast"],
+        pontos_fortes: "Versatilidade, orbes poderosas, defesa",
+        pontos_fracos: "Curva de aprendizado íngreme, dependência de orbes"
+    },
+    {
+        titulo: "Watcher",
+    descrição: "A Watcher é um ser místico que pode invocar o poder de diferentes deuses para auxiliar em suas batalhas.",
+    habilidades_iniciais: ["Strike", "Defend","Eruption", "Vigilance"],
+    pontos_fortes: "Versatilidade, cartas poderosas",
+    pontos_fracos: "Curva de aprendizado íngreme"
+    }
+];
+
